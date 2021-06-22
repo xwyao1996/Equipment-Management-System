77a130ba97e61e43c866029030f6a9a1cc984339
Tangnameless <tang0079@foxmail.com>
2021-06-22 11:16:46 +0800

    update .gitignore

--- target/classes/static/js/base.js
+++
@@ -1,5 +0,0 @@
-$(function (){
-    let mainHeight = $(window).height();
-    mainHeight = Math.max(mainHeight, parseInt($('.el-main').css('height').replace("px", "")));
-    $('.el-aside').height(mainHeight);
-})
